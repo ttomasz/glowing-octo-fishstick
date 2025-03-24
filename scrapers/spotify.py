e49ec876c4270ac1d05ef80fd412f1482e9c6af1
Tomasz Taraś <tomasztaras@outlook.com>
2025-03-24 16:17:51 +0100

    added some data cleaning to spotify scraper

--- scrapers/spotify.py
+++ scrapers/spotify.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from collections.abc import Generator
 from os import getenv
 
@@ -21,17 +22,71 @@ def extract() -> Generator[models.Song, None, None]:
             scope="user-library-read",
         ),
     )
+    logger.info("Retrieving liked songs from Spotify...")
     yield from _get_liked_songs(client=sp)
+    logger.info("Done.")
 
 
 def _get_liked_songs(client: spotipy.Spotify, offset: int = 0, limit: int = 50) -> Generator[models.Song, None, None]:
+    logger.debug("Requesting liked songs from Spotify (offset=%d, limit=%d)...", offset, limit)
     results = client.current_user_saved_tracks(limit=limit, offset=offset)
     for item in results["items"]:
         track = item["track"]
-        yield models.Song(
-            artist=track["artists"][0]["name"],
-            title=track["name"],
+        title: str = track["name"]
+        title = re.sub(r" \(?\d{4} Remaster\)?", "", title)
+        title = re.sub(r" - Remastered \d{4}", "", title)
+        title = re.sub(r" - Version \d{4}", "", title)
+        title = re.sub(r" - \d{4} Version", "", title)
+        title = re.sub(r" - \d{4} Remix", "", title)
+        title = re.sub(r" - \d{4} Digital Remaster", "", title)
+        title = re.sub(r" - \d{4} Remastered Version", "", title)
+        title = re.sub(r" \(feat\. [\w &,\.]+\)", "", title, flags=re.IGNORECASE)
+        title = re.sub(" -ed$", "", title)
+        title = (
+            title
+            .replace(' - 12" Version', "")
+            .replace(' - Special 12" Dance Mix', "")
+            .replace(" - (Original Single Mono Version)", "")
+            .replace(" - 7 inch", "")
+            .replace(" - Acoustic", "")
+            .replace(" - Edit", "")
+            .replace(" - Extended Version", "")
+            .replace(" - Full Length Version", "")
+            .replace(" - Instrumental Version", "")
+            .replace(" - Live", "")
+            .replace(" - New Stereo Mix", "")
+            .replace(" - Original Album Version", "")
+            .replace(" - Original Mix", "")
+            .replace(" - Radio Edit", "")
+            .replace(" - Radio Version", "")
+            .replace(" - Re-mastered", "")
+            .replace(" - Remaster", "")
+            .replace(" - Remastered Version", "")
+            .replace(" - Remastered", "")
+            .replace(" - Remix", "")
+            .replace(" - Single Edit", "")
+            .replace(" - Single Mix", "")
+            .replace(" - Single Version", "")
+            .replace(" - Soundtrack Version", "")
+            .replace(" -ed Version", "")
+            .replace(" (Avicii Remix)", "")
+            .replace(" (Digitally Remastered)", "")
+            .replace(" (Live)", "")
+            .replace(" (Radio Edit)", "")
+            .replace(" (Single Version)", "")
+            .replace(" [Radio Edit]", "")
+            .replace(" Radio Edit", "")
+            .strip()
+            .rstrip("-")
+            .rstrip()
         )
+        if title:
+            for a in track["artists"]:
+                artist: str = a["name"]
+                yield models.Song(
+                    artist=artist,
+                    title=title,
+                )
     # if we had some results continue requesting next pages
     if len(results["items"]) > 0:
         yield from _get_liked_songs(client=client, offset=offset + limit, limit=limit)
