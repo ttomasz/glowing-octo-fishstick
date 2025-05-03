a896be44b6f673ae1611979a6213bb6e93c39110
Tomasz Taraś <tomasztaras@outlook.com>
2025-05-03 10:15:08 +0200

    added progress bar and changed some stuff to be async

--- scrapers/ultimate_guitar.py
+++ scrapers/ultimate_guitar.py
@@ -1,63 +1,119 @@
+import asyncio
 import json
 import logging
-import time
-from collections.abc import Generator
+from collections.abc import AsyncGenerator, AsyncIterable, Generator
 from datetime import timedelta
+from string import ascii_lowercase
 
 import httpx
 from bs4 import BeautifulSoup
+from tqdm.asyncio import tarange
 
 from scrapers import models
-from scrapers.utils import http_get
+from scrapers.utils import http_get_async
 
 logger = logging.getLogger(__name__)
-URL_PATTERN = "https://www.ultimate-guitar.com/explore?order=hitstotal_desc&page={page_number}&tuning[]=1&type[]=Chords"
+ARTIST_PAGES = (
+    "0-9",
+    *ascii_lowercase,
+)
+BANDS_URL_PATTERN = "https://www.ultimate-guitar.com/bands/{prefix}{page_number}.htm"
 
 
 def parse() -> Generator[models.SongChordsLink, None, None]:
-    number_of_chords = 0
-    with httpx.Client() as client:
-        for page_number in range(1, 101):
-            parsed_data_attribute = _get_page_data(client=client, page_number=page_number)
-            chords: list[dict] = parsed_data_attribute["store"]["page"]["data"]["data"]["tabs"]
-            hits: list[dict] = parsed_data_attribute["store"]["page"]["data"]["data"]["hits"]
-            views = {hit["id"]: hit["hits"] for hit in hits}
-            for chord in chords:
-                number_of_chords += 1
-                yield models.SongChordsLink(
-                    artist=chord["artist_name"],
-                    title=chord["song_name"],
-                    url=chord["tab_url"],
-                    version=chord["version"],
-                    rating=chord["rating"],
-                    votes=chord["votes"],
-                    difficulty=chord["difficulty"] if chord["difficulty"] else None,
-                    tonality_name=chord["tonality_name"] if chord["tonality_name"] else None,
-                    views=views.get(chord["id"]),
-                )
-    logger.info("Number of urls to chords: %d", number_of_chords)
+    logger.info("Starting...")
+    results = asyncio.run(_gather_all())
+    logger.info("Number of urls to chords: %d", len(results))
+    yield from results
+    logger.info("Done.")
 
 
-def _get_page_data(
-    client: httpx.Client,
-    page_number: int,
+async def _gather_all() -> list[models.SongChordsLink]:
+    results = []
+    semaphore = asyncio.Semaphore(4)  # Limit concurrent requests
+    tasks = [_gather_chord_pages(semaphore=semaphore, prefix=prefix) for prefix in ARTIST_PAGES]
+    results = [await x for x in asyncio.as_completed(tasks)]
+    return [chord_page for group in results for chord_page in group]
+
+
+async def _gather_chord_pages(semaphore: asyncio.Semaphore, prefix: str) -> list[models.SongChordsLink]:
+    async with semaphore:
+        artist_pages_urls = _get_artist_pages_urls(prefix=prefix)
+        chord_pages = _get_chord_pages(artist_pages_urls=artist_pages_urls)
+        results = [chord_page async for chord_page in chord_pages]
+        return results
+
+
+async def _get_chord_pages(artist_pages_urls: AsyncIterable[str]) -> AsyncGenerator[models.SongChordsLink, None]:
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        async for url in artist_pages_urls:
+            data = await _get_page_data_async(client=client, url=url)
+            data = data["store"]["page"]["data"]
+            for tab in data["other_tabs"]:
+                if (
+                    tab.get("marketing_type", "") not in ("TabPro", "official")
+                    and tab["type"] == "Chords"
+                    and tab["tuning"] == "Standard"
+                ):
+                    yield models.SongChordsLink(
+                        artist=tab["artist_name"],
+                        title=tab["song_name"],
+                        url=tab["tab_url"],
+                        version=tab["version"],
+                        rating=tab["rating"] if tab["rating"] else None,
+                        votes=tab["votes"] if tab["votes"] else None,
+                        difficulty=tab["difficulty"] if tab["difficulty"] else None,
+                        tonality_name=tab["tonality_name"] if tab["tonality_name"] else None,
+                    )
+
+
+async def _get_artist_pages_urls(prefix: str) -> AsyncGenerator[str, None]:
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        async for url in _get_list_of_artist_pages_urls(client=client, prefix=prefix):
+            data = await _get_page_data_async(client=client, url=url)
+            data = data["store"]["page"]["data"]
+            for artist in data["artists"]:
+                if artist["tabscount"] > 0:
+                    artist_page_url = f"https://www.ultimate-guitar.com{artist['artist_url']}"
+                    yield artist_page_url
+
+
+async def _get_list_of_artist_pages_urls(client: httpx.AsyncClient, prefix: str) -> AsyncGenerator[str, None]:
+    first_page_url = BANDS_URL_PATTERN.format(prefix=prefix, page_number="")
+    data = await _get_page_data_async(client=client, url=first_page_url)
+    data = data["store"]["page"]["data"]
+    num_pages = data["page_count"]
+    yield first_page_url
+    for page_number in tarange(2, num_pages + 1, unit="page", desc=f"Artist pages for prefix: [{prefix}]"):
+        yield BANDS_URL_PATTERN.format(prefix=prefix, page_number=page_number)
+
+
+async def _get_page_data_async(
+    client: httpx.AsyncClient,
+    url: str,
     try_number: int = 1,
     max_tries: int = 5,
     wait_period: timedelta = timedelta(seconds=30),
 ) -> dict:
-    logger.info("Checking page number: %d (try: %d/%d)", page_number, try_number, max_tries)
+    logger.debug("Checking page: %s (try: %d/%d)", url, try_number, max_tries)
     try:
-        response = http_get(client=client, url=URL_PATTERN.format(page_number=page_number))
+        response = await http_get_async(client=client, url=url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, features="html.parser")
-        data_attribute = soup.find("body").select_one(".js-store")["data-content"]
+        data_attribute: str = soup.find("body").select_one(".js-store")["data-content"]  # type: ignore  # noqa: PGH003
         parsed_data_attribute: dict = json.loads(data_attribute)
-        if not parsed_data_attribute["store"]["page"]["data"]["data"]:
+        if not parsed_data_attribute["store"]["page"]["data"]:
             raise Exception("No data in response")
         return parsed_data_attribute
     except Exception as e:
         if try_number >= max_tries:
             raise e
         logger.exception("Error while processing page: %s. Retrying in %s.", e, wait_period)
-        time.sleep(wait_period.total_seconds())
-        return _get_page_data(page_number, try_number + 1, max_tries, wait_period)
+        await asyncio.sleep(wait_period.total_seconds())
+        return await _get_page_data_async(
+            client=client,
+            url=url,
+            try_number=try_number + 1,
+            max_tries=max_tries,
+            wait_period=wait_period,
+        )
--- scrapers/utils.py
+++ scrapers/utils.py
@@ -1,5 +1,8 @@
+import asyncio
 import logging
 import time
+from asyncio import sleep as async_sleep
+from collections.abc import AsyncGenerator, Generator
 from datetime import timedelta
 from http import HTTPStatus
 
@@ -40,3 +43,48 @@ def http_get(
         )
     response.raise_for_status()
     return response
+
+
+async def http_get_async(
+    client: httpx.AsyncClient,
+    url: str,
+    timeout_s: int = 30,
+    wait_default_delay: timedelta = timedelta(seconds=30),
+    try_number: int = 1,
+    max_tries: int = 5,
+) -> httpx.Response:
+    wait_period = wait_default_delay
+    response = await client.get(url=url, timeout=timeout_s)
+    if response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
+        retry_after_raw = response.headers.get("Retry-After")
+        if retry_after_raw:
+            logger.debug("Server sent 429 status code with Retry-After header value: %s", retry_after_raw)
+            try:
+                retry_after = timedelta(seconds=int(retry_after_raw))
+                # sometimes server sends too low value in Retry-After header
+                # so let's make sure we wait at least 30 seconds
+                wait_period = max(retry_after, wait_default_delay)
+            except ValueError:
+                pass  # if Retry-After is not a number, we will use default wait_period
+        logger.warning("Server sent 429 (Too Many Requests) status code. Going to sleep for: %s", wait_period)
+        await async_sleep(wait_period.total_seconds())
+        return await http_get_async(
+            client=client,
+            url=url,
+            timeout_s=timeout_s,
+            wait_default_delay=wait_period,
+            try_number=try_number + 1,
+            max_tries=max_tries,
+        )
+    response.raise_for_status()
+    return response
+
+
+def async_to_sync_generator(loop: asyncio.AbstractEventLoop, async_gen: AsyncGenerator) -> Generator:
+    """Convert an async generator into a regular generator."""
+    while True:
+        try:
+            task = asyncio.run_coroutine_threadsafe(async_gen.__anext__(), loop)
+            yield task.result()
+        except StopAsyncIteration:
+            break
